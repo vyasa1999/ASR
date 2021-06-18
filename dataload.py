32a06f7d5ed8cbc6fa37231c68bfb620f3cf84d1
vyasa1999 <70603071+vyasa1999@users.noreply.github.com>
2021-06-18 13:03:27 -0400

    updated

--- dataload.py
+++ dataload.py
@@ -10,19 +10,26 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
-y, sr = librosa.load("words/do20.wav", sr=44000)
+
 labels = []
-words = np.zeros((25,88000))
-for i in range(24):
-    labels.append("do")
-    if i < 9:
-        words[i], _= librosa.load("words/do" + "0" + str(i+1) + ".wav", sr=44000)
-    else:
-        words[i], _= librosa.load("words/do" + str(i+1) + ".wav", sr=44000)
+wordNames = ["a", "and", "as", "at", "be", "but", "by", "do", "for", "from", 
+             "have", "he", "her", "his", "i", "in", "it", "not", "of", "on", 
+             "or", "say", "she", "that", "the", "they", "this", "to", "we",
+             "with", "you"]
+numWords = len(wordNames) * 25
+words = np.zeros((numWords,88000))
+mfcc = np.zeros((numWords,20,172))
+for k in range(len(wordNames)):
+    print(k)
+    for i in range(25):
+        labels.append(wordNames[k])
+        if i < 9:
+            words[i], _= librosa.load("words/and" + "0" + str(i+1) + ".wav", sr=44000)
+        else:
+            words[i], _= librosa.load("words/and" + str(i+1) + ".wav", sr=44000)
         
+for i in range(numWords):
+    mfcc[i] = librosa.feature.mfcc(y=words[i], sr=44000)
 
-mfcc = librosa.feature.mfcc(y=words[0], sr=44000)
-#for i in range(24):
- #   mfcc[i] = librosa.feature.mfcc(y=words[i], sr=44000)
 
-librosa.display.specshow(mfcc, sr=sr, x_axis='time')
+print(mfcc)
